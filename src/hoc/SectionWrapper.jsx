42ca716f77db2913bb787a9e2c8609e6f6850fad
Oleg Soshko <ososhko@MACBOOK-PRO-SOSKO.local>
2023-11-20 12:19:07 +0300

    fix

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -9,7 +9,7 @@ export const SectionWrapper = (Component, id) => function HOC() {
       variants={staggerContainer()}
       initial='hidden'
       whileInView='show'
-      viewport={{ once: true, amount: 0.25 }}
+      viewport={{ once: true, amount: 0.1 }}
       className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
     >
       <span className='hash-span' id={id}>&nbsp;</span>
